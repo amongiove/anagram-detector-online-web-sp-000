26672c5ae9deba5387a29f0164e7d79f09287fa7
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-24 19:47:58 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,24 +4,26 @@ require 'pry'
 class Anagram
 
   attr_accessor :some_word
-  words = %w()
 
   def initialize(some_word)
     @some_word = some_word
   end
 
-  def match(words)
+  def match(array)
 
-    matches = [] #empty array
-    check = @some_word.split.sort #need some_word to be split into letters and sorted
-    words.each do |word| #for each word
-      array_check = word.split.sort # check = the word split into letters and sorted
-      if array_check == check # if check is the same as @some_word
-         matches << word  #add that (og)word to expty array
-      end
-    end
-    matches #return new array (empty or full)
+    #matches = [] #empty array
+    #check = @some_word.split.sort #need some_word to be split into letters and sorted
+    #words.each do |word| #for each word
+      #array_check = word.split.sort # check = the word split into letters and sorted
+      #if array_check == check # if check is the same as @some_word
+      #   matches << word  #add that (og)word to expty array
+      #end
+    #end
+    #matches #return new array (empty or full)
+  #end
+  array.collect do |word|
+    word.split("").sort == @word.split("").sort
   end
-
+end
 
 end
